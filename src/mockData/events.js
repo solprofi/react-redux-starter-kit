d71baf4ef5495aedbf340d436d4c9f00f2c030f9
Roman Sologor <solohor.roman@gmail.com>
2020-12-12 20:14:55 +0200

    Update linting config. Add meterial ui packages. Add event mock data.

---
+++ src/mockData/events.js
@@ -0,0 +1,39 @@
+const eventList = [
+  {
+    title: 'Party at Roman\'s',
+    date: new Date(),
+    id: 1,
+    participants: [],
+    description: 'This is the best event ever',
+  },
+  {
+    title: 'Party at Dimas\'s',
+    date: new Date(),
+    id: 2,
+    participants: [],
+    description: 'lorem lorem lorem lwkadslskjd',
+  },
+  {
+    title: 'Party at Vlad\'s',
+    date: new Date(),
+    id: 3,
+    participants: [],
+    description: 'lqkwe lq el qlwe',
+  },
+  {
+    title: 'Party at Slav\'s',
+    date: new Date(),
+    id: 4,
+    participants: [],
+    description: 'T qwklej kqjwne  qweln qwelqkwel qwlke ever',
+  },
+  {
+    title: 'Party at Marusia\'s',
+    date: new Date(),
+    id: 5,
+    participants: [],
+    description: 'This is the best event ever',
+  },
+];
+
+export default eventList;
